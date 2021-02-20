eec7fd0bd46dce1b27ad4f50fe665ec65df9805a
Hamster <chensuisheng@dasurebao.com>
2021-02-20 22:07:50 +0800

    init

---
+++ kylms-api/kylms-api-system/src/main/java/com/kaiyu56/system/api/RemoteFileService.java
@@ -0,0 +1,29 @@
+package com.kaiyu56.system.api;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+import com.kaiyu56.common.core.constant.ServiceNameConstants;
+import com.kaiyu56.common.core.domain.R;
+import com.kaiyu56.system.api.domain.SysFile;
+import com.kaiyu56.system.api.factory.RemoteFileFallbackFactory;
+
+/**
+ * 文件服务
+ * 
+ * @author ruoyi
+ */
+@FeignClient(contextId = "remoteFileService", value = ServiceNameConstants.FILE_SERVICE, fallbackFactory = RemoteFileFallbackFactory.class)
+public interface RemoteFileService
+{
+    /**
+     * 上传文件
+     *
+     * @param file 文件信息
+     * @return 结果
+     */
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public R<SysFile> upload(@RequestPart(value = "file") MultipartFile file);
+}
---
+++ kylms-gateway/src/main/java/com/kaiyu56/gateway/config/GatewayConfig.java
@@ -0,0 +1,32 @@
+package com.kaiyu56.gateway.config;
+
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.SentinelGatewayFilter;
+import com.kaiyu56.gateway.handler.SentinelFallbackHandler;
+
+/**
+ * 网关限流配置
+ * 
+ * @author ruoyi
+ */
+@Configuration
+public class GatewayConfig
+{
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SentinelFallbackHandler sentinelGatewayExceptionHandler()
+    {
+        return new SentinelFallbackHandler();
+    }
+
+    @Bean
+    @Order(-1)
+    public GlobalFilter sentinelGatewayFilter()
+    {
+        return new SentinelGatewayFilter();
+    }
+}
